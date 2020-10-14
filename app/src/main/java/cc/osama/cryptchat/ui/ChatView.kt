6587c1253286059980a6050b342094006823a751
OsamaSayegh <asooomaasoooma90@gmail.com>
2020-10-14 11:24:37 +0300

    Introduce small informational messages in chat view

--- app/src/main/java/cc/osama/cryptchat/ui/ChatView.kt
+++ app/src/main/java/cc/osama/cryptchat/ui/ChatView.kt
@@ -19,19 +19,21 @@ import cc.osama.cryptchat.db.Message
 import cc.osama.cryptchat.db.Server
 import cc.osama.cryptchat.db.User
 import kotlinx.android.synthetic.main.activity_chat_view.*
+import kotlinx.android.synthetic.main.chat_system_message.view.*
 import kotlinx.android.synthetic.main.sent_chat_message.view.*
 import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.collections.ArrayList
 
-class ChatView : RecyclerViewImplementer<ChatView.DisplayMessageStruct>() {
-  class DisplayMessageStruct(
+class ChatView : RecyclerViewImplementer<ChatView.ChatRowItem>() {
+  interface ChatRowItem
+  class ChatMessage(
     val plaintext: String,
     val id: Long,
     val status: Int,
     val createdAt: Long,
     val byMe: Boolean
-  ) {
+  ) : ChatRowItem {
     constructor(message: Message) : this(
       id = message.id,
       plaintext = message.plaintext,
@@ -40,7 +42,9 @@ class ChatView : RecyclerViewImplementer<ChatView.DisplayMessageStruct>() {
       byMe = message.byMe()
     )
   }
-  override val dataset = ArrayList<DisplayMessageStruct>()
+  class ChatSystemMessage(val content: String) : ChatRowItem
+
+  override val dataset = ArrayList<ChatRowItem>()
   override val defaultLayout = R.layout.sent_chat_message
   override val viewAdapter = Adapter(dataset, defaultLayout, this)
   override val viewManager = LinearLayoutManager(this).apply {
@@ -178,6 +182,7 @@ class ChatView : RecyclerViewImplementer<ChatView.DisplayMessageStruct>() {
         }
       }
     }
+    dataset.add(ChatSystemMessage(resources.getString(R.string.chat_view_chat_beginning)))
   }
 
   override fun onStop() {
@@ -204,14 +209,54 @@ class ChatView : RecyclerViewImplementer<ChatView.DisplayMessageStruct>() {
     AsyncExec.run(AsyncExec.Companion.Threads.Db) {
       val scrollToBottom = viewManager.findLastVisibleItemPosition() + 1 == dataset.size
       synchronized(dataset) {
-        val lastId = dataset.maxBy { m -> m.id }?.id ?: 0
+        val lastId = dataset.filterIsInstance<ChatMessage>().maxBy { m -> m.id }?.id ?: 0
         val messages = db().messages().findConversationMessages(
           serverId = server.id,
           userId = user.id,
           lastId = lastId
         )
         if (messages.isEmpty()) return@run
-        dataset.addAll(messages.map { m -> DisplayMessageStruct(m) })
+        messages.forEachIndexed { index, message ->
+          val msgDate = Calendar.getInstance()
+          msgDate.time = Date(message.createdAt)
+          val today = Calendar.getInstance()
+          today.time = Date(System.currentTimeMillis())
+          val yesterday = Calendar.getInstance()
+          yesterday.time = Date(System.currentTimeMillis() - (3600 * 24 * 1000))
+          val formatter = SimpleDateFormat("yyyy/MM/dd", Locale.getDefault())
+          var prevMsgDate: Calendar?
+          prevMsgDate = Calendar.getInstance()
+          if (index > 0) {
+            prevMsgDate?.time = Date(messages[index - 1].createdAt)
+          } else if (dataset.size > 0) {
+            val prev = dataset[dataset.size - 1]
+            if (prev is ChatMessage) prevMsgDate?.time = Date(prev.createdAt) else prevMsgDate = null
+          } else {
+            prevMsgDate = null
+          }
+          if (prevMsgDate == null) {
+            if (datesOnSameDay(msgDate, yesterday)) {
+              dataset.add(ChatSystemMessage(resources.getString(R.string.chat_view_yesterday)))
+            } else if (!datesOnSameDay(msgDate, today)) {
+              dataset.add(ChatSystemMessage(formatter.format(msgDate.time)))
+            }
+          } else {
+            if (!datesOnSameDay(msgDate, prevMsgDate)) {
+              when {
+                datesOnSameDay(msgDate, today) -> {
+                  dataset.add(ChatSystemMessage(resources.getString(R.string.chat_view_today)))
+                }
+                datesOnSameDay(msgDate, yesterday) -> {
+                  dataset.add(ChatSystemMessage(resources.getString(R.string.chat_view_yesterday)))
+                }
+                else -> {
+                  dataset.add(ChatSystemMessage(formatter.format(msgDate.time)))
+                }
+              }
+            }
+          }
+          dataset.add(ChatMessage(message))
+        }
         if (!Cryptchat.isReadonly(applicationContext)) {
           db().messages().setMessagesReadByServerAndUser(serverId = server.id, userId = user.id)
         }
@@ -231,9 +276,9 @@ class ChatView : RecyclerViewImplementer<ChatView.DisplayMessageStruct>() {
       val message = db().messages().findById(messageId) ?: return@run
       AsyncExec.onUiThread {
         synchronized(dataset) {
-          val index = dataset.indexOfFirst { m -> m.id == messageId }
+          val index = dataset.indexOfFirst { m -> m is ChatMessage && m.id == messageId }
           if (index != -1) {
-            dataset[index] = DisplayMessageStruct(message)
+            dataset[index] = ChatMessage(message)
             viewAdapter.notifyItemChanged(index)
           }
         }
@@ -243,34 +288,43 @@ class ChatView : RecyclerViewImplementer<ChatView.DisplayMessageStruct>() {
 
   override fun onBindViewHolder(holder: Adapter.ListItemHolder, position: Int) {
     val item = dataset[position]
-    holder.view.messageTextHolder.text = item.plaintext
-    SimpleDateFormat("HH:mm", Locale.getDefault()).also { formatter ->
-      holder.view.messageDateHolder.text = formatter.format(Date(item.createdAt))
-    }
-    if (item.byMe) {
-      val icon = when (item.status) {
-        Message.INITIAL_STATE -> {
-          R.drawable.ic_hourglass_empty_black_24dp
-        }
-        Message.NEEDS_RETRY -> {
-          R.drawable.ic_hourglass_empty_black_24dp
-        }
-        Message.SENT -> {
-          R.drawable.ic_check_black_24dp
-        }
-        else -> {
-          R.drawable.ic_error_outline_black_24dp
-        }
+    if (item is ChatMessage) {
+      holder.view.messageTextHolder.text = item.plaintext
+      SimpleDateFormat("HH:mm", Locale.getDefault()).also { formatter ->
+        holder.view.messageDateHolder.text = formatter.format(Date(item.createdAt))
       }
-      holder.view.messageStatusIcon.setImageResource(icon)
+      if (item.byMe) {
+        val icon = when (item.status) {
+          Message.INITIAL_STATE -> {
+            R.drawable.ic_hourglass_empty_black_24dp
+          }
+          Message.NEEDS_RETRY -> {
+            R.drawable.ic_hourglass_empty_black_24dp
+          }
+          Message.SENT -> {
+            R.drawable.ic_check_black_24dp
+          }
+          else -> {
+            R.drawable.ic_error_outline_black_24dp
+          }
+        }
+        holder.view.messageStatusIcon.setImageResource(icon)
+      }
+    } else if (item is ChatSystemMessage) {
+      holder.view.systemMessageHolder.text = item.content
     }
   }
 
   override fun getItemViewType(position: Int): Int {
-    return if (dataset[position].byMe) {
-      super.getItemViewType(position)
+    val item = dataset[position]
+    return if (item is ChatMessage) {
+      if (item.byMe) {
+        super.getItemViewType(position)
+      } else {
+        R.layout.received_chat_message
+      }
     } else {
-      R.layout.received_chat_message
+      R.layout.chat_system_message
     }
   }
 
@@ -295,4 +349,9 @@ class ChatView : RecyclerViewImplementer<ChatView.DisplayMessageStruct>() {
   }
 
   private fun db() = Cryptchat.db(applicationContext)
+
+  private fun datesOnSameDay(d1: Calendar, d2: Calendar) : Boolean {
+    return d1.get(Calendar.DAY_OF_YEAR) == d2.get(Calendar.DAY_OF_YEAR) &&
+      d1.get(Calendar.YEAR) == d2.get(Calendar.YEAR)
+  }
 }
